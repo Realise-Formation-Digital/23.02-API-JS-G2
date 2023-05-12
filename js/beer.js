dbedfdd44f1ab805d5c3586b703d75693890ab6e
BaoBaB-laBs <126256031+BaoBaB-laBs@users.noreply.github.com>
2023-05-12 16:26:30 +0200

    first step to display Beer Infos

--- js/beer.js
+++ js/beer.js
@@ -1,3 +1,102 @@
 import { API_BASE_URL } from "../constants/constants.js";
 
-console.log(API_BASE_URL);
+async function displayData() {
+  try {
+    const res = await axios.get(API_BASE_URL + "beers/4");
+    console.log(res);
+    const beerDetails = res.data;
+    console.log(beerDetails);
+
+    // Vérifier si beerDetails est une liste ou une seule valeur
+    if (Array.isArray(beerDetails)) {
+      // Si beerDetails est une liste, itérer à travers les éléments
+      const colElement = document.getElementById("listeBeerIngredients");
+      for (let tout of beerDetails) {
+        // Créer et ajouter les éléments HTML pour chaque élément
+        const divEl = createBeerElement(tout);
+        colElement.appendChild(divEl);
+      }
+    } else {
+      // Si beerDetails est une seule valeur, créer et ajouter l'élément HTML directement
+      const colElement = document.getElementById("listeBeerIngredients");
+      const divEl = createBeerElement(beerDetails);
+      colElement.appendChild(divEl);
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+function createBeerElement(beer) {
+  const divEl = document.createElement("div");
+  const modalTitleEl = document.getElementById("modalLabel");
+  modalTitleEl.textContent = beer.name + " - Since : " + beer.first_brewed;
+
+  divEl.innerHTML = `
+      <div class="card" style="width:100%;">
+        <div class="card-body">
+          <div class="row">
+            <div class="col text-center">
+            <img src=${beer.image_url} alt="Beer Picture" style="width:80px;height:auto;" class="center">
+          </div>
+          <div class="col">
+          <h5 class="card-title">${beer.description}</h5>
+        </div>
+        <p class="card-text">${beer.brewers_tips}</p>
+      </div>
+    
+ 
+    `;
+  const ingredientsDiv = createBeerIngredients(beer);
+  divEl.appendChild(ingredientsDiv);
+  divEl.classList.add("col");
+  divEl.addEventListener("click", function afficherIngredients() {
+    alert(beer.name);
+  });
+  return divEl;
+}
+
+function createBeerIngredients(beer) {
+  const divEl = document.createElement("div");
+  divEl.innerHTML = `
+      <div class="row">
+        <div class="col">
+          <div class="card">
+            <div class="card-header">
+              Malt
+            </div>
+            <div class="card-body bg-success">
+              <ul>
+                ${beer.ingredients.malt
+                  .map(
+                    (malt) =>
+                      `<li>${malt.name}: ${malt.amount.value} ${malt.amount.unit}</li>`
+                  )
+                  .join("")}
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <div class="card-header">
+              Hops
+            </div>
+            <div class="card-body bg-info">
+              <ul>
+                ${beer.ingredients.hops
+                  .map(
+                    (hop) =>
+                      `<li>${hop.name}: ${hop.amount.value} ${hop.amount.unit} - ${hop.add}</li>`
+                  )
+                  .join("")}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  return divEl;
+}
+
+displayData();
